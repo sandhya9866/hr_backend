d3d24dc5f45117c54e5715ee696c8f4deb0ddae7
amritab3 <shellybhattarai@gmail.com>
2025-05-26 17:29:12 +0545

    edit delete feature

--- department/views.py
+++ department/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect
 from .models import Department
 from .forms import DepartmentForm
 
@@ -13,7 +14,7 @@ class DepartmentListView(LoginRequiredMixin, ListView):
     context_object_name = 'departments'
 
     def get_queryset(self):
-        return Department.objects.all().prefetch_related('department_head')
+        return Department.objects.all().prefetch_related('department_head').order_by('-created_on')
 
 class DepartmentCreateView(LoginRequiredMixin, CreateView):
     model = Department
@@ -33,22 +34,28 @@ class DepartmentCreateView(LoginRequiredMixin, CreateView):
 class DepartmentUpdateView(LoginRequiredMixin, UpdateView):
     model = Department
     form_class = DepartmentForm
-    template_name = 'department/department_form.html'
+    template_name = 'department/department_create.html'
     success_url = reverse_lazy('department:department_list')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['action'] = 'Update'
+        context['action'] = 'Edit'
         return context
 
     def form_valid(self, form):
+        response = super().form_valid(form)
         messages.success(self.request, 'Department updated successfully!')
-        return super().form_valid(form)
+        return response
 
 class DepartmentDeleteView(LoginRequiredMixin, DeleteView):
     model = Department
     success_url = reverse_lazy('department:department_list')
 
-    def delete(self, request, *args, **kwargs):
-        messages.success(request, 'Department deleted successfully!')
-        return super().delete(request, *args, **kwargs)
+    def get_object(self, queryset=None):
+        return get_object_or_404(Department, pk=self.kwargs['pk'])
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        messages.success(request, "Department deleted successfully.")
+        return redirect(self.success_url)
