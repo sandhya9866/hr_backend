18bd4b740827a5b7781590034528861a254bf3ca
amritab3 <shellybhattarai@gmail.com>
2025-05-27 16:14:54 +0545

    add filter on department list page

--- department/views.py
+++ department/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect
+from django.db.models import Q
 from .models import Department
+from user.models import AuthUser
 from .forms import DepartmentForm
 
 # Create your views here.
@@ -12,9 +14,40 @@ class DepartmentListView(LoginRequiredMixin, ListView):
     model = Department
     template_name = 'department/department_list.html'
     context_object_name = 'departments'
+    paginate_by = 3
+
+    def get(self, request, *args, **kwargs):
+        if request.GET.get('reset'):
+            request.session.pop('department_name', None)
+            request.session.pop('department_head', None)
+            return redirect('department:department_list')
+        return super().get(request, *args, **kwargs)
 
     def get_queryset(self):
-        return Department.objects.all().prefetch_related('department_head').order_by('-created_on')
+        queryset = Department.objects.all().prefetch_related('department_head')
+        name = self.request.session.get('department_name', '')
+        department_head = self.request.session.get('department_head', '')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if department_head:
+            queryset = queryset.filter(department_head__id=department_head).distinct()
+        
+        return queryset.order_by('-created_on')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_departments'] = Department.objects.all().order_by('name')
+        context['all_users'] = AuthUser.objects.filter(department_head__isnull=False).distinct().order_by('first_name')
+        context['name'] = self.request.session.get('department_name', '')
+        context['department_head'] = self.request.session.get('department_head', '')
+        return context
+
+    def post(self, request, *args, **kwargs):
+        request.session['department_name'] = request.POST.get('name', '')
+        request.session['department_head'] = request.POST.get('department_head', '')
+        return self.get(request, *args, **kwargs)
+
 
 class DepartmentCreateView(LoginRequiredMixin, CreateView):
     model = Department
@@ -36,6 +69,7 @@ class DepartmentUpdateView(LoginRequiredMixin, UpdateView):
     form_class = DepartmentForm
     template_name = 'department/department_create.html'
     success_url = reverse_lazy('department:department_list')
+    
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
