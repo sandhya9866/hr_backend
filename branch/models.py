9c9c438d4d18ba94e24eae88c821064da959af82
amritab3 <shellybhattarai@gmail.com>
2025-06-20 18:05:40 +0545

    create model for province and district, add fixtures

--- branch/models.py
+++ branch/models.py
@@ -12,3 +12,21 @@ class Branch(models.Model):
     class Meta:
         verbose_name = 'Branch'
         verbose_name_plural = 'Branches'
+
+class Province(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=10, unique=True, blank=True, null=True)
+    cbs_code = models.IntegerField(null=True, blank=True)
+
+class District(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    cbs_code = models.IntegerField(null=True, blank=True)
+    code = models.CharField(max_length=10, unique=True, blank=True, null=True)
+    province = models.ForeignKey(
+        "Province", related_name="districts", on_delete=models.CASCADE
+    )
+
+def __str__(self):
+    return str(self.name)
